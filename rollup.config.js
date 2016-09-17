99c2e5c49a5a3fe4b4ef7e3d9ff47c2dd624dc78
Josh Milo <josh@joshmilo.com>
2016-09-17 13:36:51 -0700

    Adds environment variable resolution and node compat

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,26 @@
+import babel from 'rollup-plugin-babel';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import replace from 'rollup-plugin-replace';
+
 export default {
   entry: 'src/index.js',
   dest: 'build/js/index.min.js',
   format: 'iife',
   sourceMap: 'inline',
   plugins: [
+    resolve({
+      jsnext: true,
+      main: true,
+      browser: true,
+    }),
+    commonjs(),
     babel({
       exclude: 'node_modules/**',
+    }),
+    replace({
+      exclude: 'node_modules/**',
+      ENV: JSON.stringify(process.env.NODE_ENV || 'development'),
     }),
   ]
 };
--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log("hello");
+class Point {
+  constructor(x,y) {
+    this.x = x;
+    this.y = y;
+  }
+  log() {
+    console.log(`${this.x}, ${this.y}`);
+  }
+}
